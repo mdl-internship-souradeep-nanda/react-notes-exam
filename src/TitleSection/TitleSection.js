41f9784119a4b3da223fcc453d93e59681440c7c
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-14 10:26:22 +0530

    TitleSection without css

--- src/TitleSection/TitleSection.js
+++ src/TitleSection/TitleSection.js
@@ -5,8 +5,22 @@ import STRINGS from '../strings.json';
 
 function TitleSection() {
   return (
-    <div>
-      {STRINGS.note_title}
+    <div className="wrapper">
+      <div className="title-bar">
+        <div className="title">
+          {STRINGS.note_title}
+        </div>
+        <button className="language-button">
+          {STRINGS.language}
+        </button>
+      </div>
+      <div className="input-wrapper">
+        <input
+          className="note-title"
+          type="text"
+          placeholder={STRINGS.title_placeholder}
+        />
+      </div>
     </div>
   );
 }
